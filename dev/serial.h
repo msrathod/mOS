86f4775114032b55de4e426e8629d66c68a5dc65
Mohit Rathod <msrathod@users.noreply.github.com>
2022-09-17 17:37:33 +0530

    UART driver

--- dev/serial.h
+++ dev/serial.h
@@ -3,13 +3,50 @@
  * @author 	Mohit Rathod
  * Created: 17 09 2022, 07:51:28 am
  * -----
- * Last Modified: 17 09 2022, 07:51:28 am
+ * Last Modified: 17 09 2022, 05:25:19 pm
  * Modified By  : Mohit Rathod
  * -----
  * MIT License
  * https://opensource.org/licenses/MIT
  * 
- * @brief 
+ * @brief Serial (UART) driver for mOS
  * 
  */
+#ifndef driver_serial_h
+#define driver_serial_h
+#include <stdint.h>
 
+/**
+ * @fn      int serial_init(void);
+ * @brief   Initialize the UART peripheral on the MCU.
+ * @param   void
+ * @return  0 on success, -1 otherwise
+ */
+int serial_init(void);
+
+/**
+ * @fn      int serial_getchar(void);
+ * @brief   Read a character from Serial(UART) module.
+ * @param   void
+ * @return  read char on success, -1 otherwise
+ */
+int serial_getchar(void);
+
+/**
+ * @fn      int serial_putchar(int c);
+ * @brief   Write a character to Serial(UART) module.
+ * @param   c   character to write 
+ * @return  0 on success, -1 otherwise
+ */
+int serial_putchar(int c);
+
+/**
+ * @fn      int serial_write(const char *str);
+ * @brief   Write a string literal to Serial(UART) module.
+ * @param   str string to write 
+ * @return  0 on success, -1 otherwise
+ */
+int serial_write(const char *str);
+
+
+#endif /* driver_serial_h */
---
+++ mOS/mcu.h
@@ -0,0 +1,20 @@
+/** 
+ * @file 	mcu.h
+ * @author 	Mohit Rathod
+ * Created: 17 09 2022, 05:26:54 pm
+ * -----
+ * Last Modified: 17 09 2022, 05:36:07 pm
+ * Modified By  : Mohit Rathod
+ * -----
+ * MIT License
+ * https://opensource.org/licenses/MIT
+ * 
+ * @brief 
+ * 
+ */
+#ifndef mcu_mos_h
+#define mcu_mos_h
+
+#include <msp430g2553.h>
+
+#endif /* mcu_mos_h */
