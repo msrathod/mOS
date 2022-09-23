127d3a08a6af125abbd803aba6c24a7d4d17bc09
Mohit Rathod <msrathod@users.noreply.github.com>
2022-09-23 23:16:13 +0530

    Update i2c.h

--- dev/i2c.h
+++ dev/i2c.h
@@ -3,7 +3,7 @@
  * @author 	Mohit Rathod
  * Created: 23 09 2022, 10:20:51 pm
  * -----
- * Last Modified: 23 09 2022, 10:44:08 pm
+ * Last Modified: 23 09 2022, 11:10:01 pm
  * Modified By  : Mohit Rathod
  * -----
  * MIT License
@@ -18,9 +18,9 @@
 #include <mosconfig.h>
 
 #if MOS_USES(I2C) == 2
-#include <i2c_slave.h>
+#include <i2cslave.h>
 #elif MOS_USES(I2C) == 1
-#include <i2c_master.h>
+#include <i2cmaster.h>
 #endif
 
 #endif /* dev_i2c_h */
