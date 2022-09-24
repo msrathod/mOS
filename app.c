d287e3779e93c8a5abd5830f9c8d6a904902f13b
Mohit Rathod <msrathod@users.noreply.github.com>
2022-09-24 22:40:53 +0530

    Update app.c

--- app.c
+++ app.c
@@ -3,13 +3,13 @@
  * @author 	Mohit Rathod
  * Created: 24 09 2022, 05:52:24 pm
  * -----
- * Last Modified: 24 09 2022, 05:53:31 pm
+ * Last Modified: 24 09 2022, 10:11:21 pm
  * Modified By  : Mohit Rathod
  * -----
  * MIT License
  * https://opensource.org/licenses/MIT
  * 
- * @brief 
+ * @brief   User Application
  * 
  */
 
