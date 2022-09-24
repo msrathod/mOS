9e1b7389cc69ce7c4695ca732826b5ab5e0f253a
Mohit Rathod <msrathod@users.noreply.github.com>
2022-09-24 18:11:18 +0530

    Create app.c

---
+++ app.c
@@ -0,0 +1,26 @@
+/** 
+ * @file 	app.c
+ * @author 	Mohit Rathod
+ * Created: 24 09 2022, 05:52:24 pm
+ * -----
+ * Last Modified: 24 09 2022, 05:53:31 pm
+ * Modified By  : Mohit Rathod
+ * -----
+ * MIT License
+ * https://opensource.org/licenses/MIT
+ * 
+ * @brief 
+ * 
+ */
+
+#include <mos.h>
+
+void setup()
+{
+    ;
+}
+
+void loop()
+{
+    ;
+}
