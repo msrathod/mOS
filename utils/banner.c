9a98ab79d6072fcc0e7f7ec008a63fb6b09ca968
Mohit Rathod <msrathod@users.noreply.github.com>
2022-09-24 16:30:34 +0530

    Update banner.c

--- utils/banner.c
+++ utils/banner.c
@@ -3,7 +3,7 @@
  * @author 	Mohit Rathod
  * Created: 17 09 2022, 04:44:13 pm
  * -----
- * Last Modified: 17 09 2022, 04:49:55 pm
+ * Last Modified: 24 09 2022, 03:47:38 pm
  * Modified By  : Mohit Rathod
  * -----
  * MIT License
@@ -14,12 +14,32 @@
  */
 #include <mosprint.h>
 #include <utils/banner.h>
+#include <dev/clock.h>
+#if MOS_USES(UART)
+#include <dev/serial.h>
+#endif
+#ifdef MOS_USES(MD13S)
+#include <dev/md13s.h>
+#endif
 
 void mOSgreet()
 {
+    int tmp;
     OPRINT("\n       _____ _____ ", NULL);
     OPRINT("\n _____|     |   __|", NULL);
     OPRINT("\n|     |  |  |__   |", NULL);
     OPRINT("\n|_|_|_|_____|_____|", NULL);
     MPRINT("\nWelcome to mOS!\t The mini OS for msp430g2xxx. \a\n");
+    tmp = getMCLK();
+    OPRINT("\nClock [Main]:\t", &tmp);OPRINT(" MHz", NULL);
+    tmp = getACLK();
+    OPRINT("\nClock [Aux]:\t", &tmp);OPRINT(" Hz", NULL);
+#if MOS_USES(UART)
+    tmp = getSerialBaud();
+    OPRINT("\nBaud rate:\t", &tmp);OPRINT("00 bps", NULL);
+#endif
+#ifdef MOS_USES(MD13S)
+    tmp = getMD13sFreq();
+    OPRINT("\nMD13s PWM freq:\t", &tmp);OPRINT(" Hz", NULL);
+#endif
 }
