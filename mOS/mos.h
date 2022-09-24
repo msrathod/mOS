14a7cccb97b8d78e92a631b063d01431ac5188d2
Mohit Rathod <msrathod@users.noreply.github.com>
2022-09-24 16:30:30 +0530

    Update mos.h

--- mOS/mos.h
+++ mOS/mos.h
@@ -3,15 +3,13 @@
  * @author 	Mohit Rathod
  * Created: 16 09 2022, 05:06:20 pm
  * -----
- * Last Modified: 24 09 2022, 08:00:55 am
+ * Last Modified: 24 09 2022, 08:42:28 am
  * Modified By  : Mohit Rathod
  * -----
  * MIT License
  * https://opensource.org/licenses/MIT
  * 
  * @brief   mOS - The mini Operating System includes.
- *          Provides all the modules configured in the 
- *          mosconfig file. 
  */
 #ifndef mos_include_h
 #define mos_include_h
