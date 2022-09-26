648edfad69c4088fd4779293a3683125f52d8afd
Mohit Rathod <msrathod@users.noreply.github.com>
2022-09-26 15:56:21 +0530

    Create smf.c

---
+++ utils/smf.c
@@ -0,0 +1,93 @@
+/** 
+ * @file 	smf.c
+ * @author 	Mohit Rathod
+ * Created: 26 09 2022, 01:49:19 pm
+ * -----
+ * Last Modified: 26 09 2022, 03:41:25 pm
+ * Modified By  : Mohit Rathod
+ * -----
+ * MIT License
+ * https://opensource.org/licenses/MIT
+ * 
+ * @brief   State Machine Framework API source
+ * 
+ */
+#include "smf.h"
+#include <utils/queue.h>
+
+typedef struct
+{
+    pAction_t action[MaxStates][MaxEvents];
+} stateMachine_t;
+
+
+#define EVENT_QUEUE_LEN         (8)
+
+static qid_t event_qID;
+static uint8_t event_qMEM[EVENT_QUEUE_LEN];
+static state_t _smState;
+static stateMachine_t SM;
+
+
+static int SMF_getEvent(qid_t qID, event_t *pEvent);
+static state_t SMF_transition(state_t uState, event_t uEvent);
+
+int SMF_init(state_t iState)
+{
+    q_attr_t attr = {
+        .elen = (event_qMEM[0]),
+        .qlen = ARRAY_SIZE(event_qMEM),
+        .buffer = event_qMEM
+    };
+    /* set the intitial state of the state machine */
+    _smState = iState;
+    return q_init(&event_qID, &attr);
+}
+
+int SMF_addState(state_t uState, stateTransition_t *ptr)
+{
+    int ret = -1;
+    if ((ptr != NULL) && (uState >= STATE_0) && (uState < MAX_States)) {
+        int len = ptr->len;
+        while (len--) {
+            SM.action[uState - STATE_0][((ptr->pAction+len)->event) - EVENT_0] 
+                                                = (ptr->pAction+len)->action;
+        }
+        ret = 0;
+    }
+    return ret;
+}
+
+state_t SMF_getState()
+{
+    return _smState;
+}
+
+int SMF_putEvent(const uint8_t *pEvent)
+{
+    return qEnqueue(event_qID, pEvent);
+}
+
+void SMF_Run()
+{
+    event_t uEvent;
+    if (SMF_getEvent(event_qID, &uEvent) == 0) {
+        _smState = SMF_transition( _smState, uEvent);
+    }
+}
+
+static int SMF_getEvent(qid_t qID, event_t *pEvent)
+{
+    uint8_t tmp;
+    int ret = qDequeue(qID, &tmp);
+    *pEvent = (event_t)tmp;
+    return ret;
+}
+
+static state_t SMF_transition(state_t uState, event_t uEvent)
+{
+    if (SM.action[uState - STATE_0][uEvent - EVENT_0] != NULL) {
+        uState = SM.action[uState - STATE_0][uEvent - EVENT_0]();
+    }
+    return uState;
+}
