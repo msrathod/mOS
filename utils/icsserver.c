e6f3f5b21267ef900dd6435216bf433746536631
Mohit Rathod <msrathod@users.noreply.github.com>
2022-10-11 23:50:44 +0530

    Update icsserver.c

--- utils/icsserver.c
+++ utils/icsserver.c
@@ -3,7 +3,7 @@
  * @author 	Mohit Rathod
  * Created: 10 10 2022, 09:56:18 pm
  * -----
- * Last Modified: 11 10 2022, 02:18:20 pm
+ * Last Modified: 11 10 2022, 10:35:55 pm
  * Modified By  : Mohit Rathod
  * -----
  * MIT License
@@ -16,6 +16,7 @@
 #include <dev/i2c.h>
 #include <utils/crc8.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 /* Service format */
 typedef struct
@@ -27,8 +28,21 @@ typedef struct
 	volatile uint16_t run;
 } services_t;
 
+typedef enum serverState
+{
+	BAD_FRAME = 0x0F,
+	HEADER_MODE,
+	LENGTH_MODE,
+	PORT_MODE,
+	DATA_MODE,
+	CRC_MODE,
+	PKT_RSP_MODE,
+	SRVC_RSP_MODE
+} Serverstate_t;
+
 static services_t i2csrvc[PORT_NUM_MAX];
 
+static Serverstate_t state;
 static ISMPframe_t packet;
 static ISMPresponse_t response = UNKNOWN_RESP;
 
@@ -38,6 +52,7 @@ static void stateCallback(void);
 static void txCallback(volatile uint8_t *txdata);
 static void rxCallback(const uint8_t recvd);
 static void pushpacket(void);
+static bool isValidPort(ISMPport_t portNum);
 
 int ICSserver_init()
 {
@@ -53,13 +68,15 @@ int ICSserver_init()
 int ICS_addService(srvfn_t pService, const uint16_t len, void *pbuf, ISMPport_t portID)
 {
 	int ret = -1;
-  	if ((i2csrvc[portID - PORT_0].pService == NULL) && (pService != NULL)) {
-		i2csrvc[portID - PORT_0].pService = pService;
-		i2csrvc[portID - PORT_0].param = pbuf;
-		i2csrvc[portID - PORT_0].len = len;
-		i2csrvc[portID - PORT_0].rsp = UNKNOWN_RESP;
-		i2csrvc[portID - PORT_0].run = 0;
-		ret = 0;
+	if ((portID >= PORT_0) && (portID < (PORT_0 + PORT_NUM_MAX))) {
+		if ((i2csrvc[portID - PORT_0].pService == NULL) && (pService != NULL)) {
+			i2csrvc[portID - PORT_0].pService = pService;
+			i2csrvc[portID - PORT_0].param = pbuf;
+			i2csrvc[portID - PORT_0].len = len;
+			i2csrvc[portID - PORT_0].rsp = UNKNOWN_RESP;
+			i2csrvc[portID - PORT_0].run = 0;
+			ret = 0;
+		}
 	}
 	return ret;
 }
@@ -89,4 +106,51 @@ void ICS_run()
 			}
 		}
 	}
+}
+
+static void stateCallback(void)
+{
+
+}
+static void txCallback(volatile uint8_t *txdata);
+static void rxCallback(const uint8_t recvd)
+{
+	static int idx = 0;
+	uint8_t rxdata = recvd;
+	switch(state)
+	{
+		case(HEADER_MODE):
+			/* If a portID is sent to enquire its service response. */
+			if (isValidPort(rxdata)) {
+				state = SRVC_RSP_MODE;
+				rsp_port = rxdata;
+			}
+			/* or if current server state is requested */
+			else if (rxdata == ISMP_RSP_HEADER) {
+				state = PKT_RSP_MODE;
+			}
+			/* if a ISMP service header is sent to indicate an ISMP frame */
+			else if (rxdata == ISMP_SVC_HEADER) {
+				state = LENGTH_MODE;
+				packet.Header = ISMP_SVC_HEADER;
+				response = ISMP_ONGOING;
+			}
+			else {
+				state = BAD_FRAME;
+				response = HEADER_ERROR;
+			}
+		break;
+		case(LENGTH_MODE):
+
+		break;
+	}
+}
+static void pushpacket(void);
+static bool isValidPort(ISMPport_t portNum)
+{
+	bool ret = false;
+	if ((portNum >= PORT_0) && (portNum < (PORT_0 + PORT_NUM_MAX))) {
+		ret = true;
+	}
+	return ret;
 }
