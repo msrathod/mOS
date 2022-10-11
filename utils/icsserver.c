3cbc1627603ddefe07b9fa2c5ac51828ae4b4101
Mohit Rathod <msrathod@users.noreply.github.com>
2022-10-11 14:19:46 +0530

    Update icsserver.c

--- utils/icsserver.c
+++ utils/icsserver.c
@@ -3,7 +3,7 @@
  * @author 	Mohit Rathod
  * Created: 10 10 2022, 09:56:18 pm
  * -----
- * Last Modified: 10 10 2022, 10:55:33 pm
+ * Last Modified: 11 10 2022, 02:18:20 pm
  * Modified By  : Mohit Rathod
  * -----
  * MIT License
@@ -13,16 +13,19 @@
  * 
  */
 #include "icsserver.h"
+#include <dev/i2c.h>
+#include <utils/crc8.h>
+#include <stddef.h>
 
 /* Service format */
 typedef struct
 {
-  srvfn_t pService;
-  void *param;
-  uint8_t len;
-  uint8_t rsp;
-  volatile uint16_t run;
-}services_t;
+	srvfn_t pService;
+	void *param;
+	uint8_t len;
+	uint8_t rsp;
+	volatile uint16_t run;
+} services_t;
 
 static services_t i2csrvc[PORT_NUM_MAX];
 
@@ -39,9 +42,51 @@ static void pushpacket(void);
 int ICSserver_init()
 {
     ISMPport_t portID;
-    for (portID = PORT_0; portID < PORT_0 + PORT_NUM_MAX; portID++) {
+    for (portID = PORT_0; portID < (PORT_0 + PORT_NUM_MAX); portID++) {
         ICS_delService(portID);
     }
     /* Enable the i2c dev in slave mode. */
+    i2cslave_init(stateCallback, txCallback, rxCallback, ICS_SERVER_ADDRESS);
     return 0;
+}
+
+int ICS_addService(srvfn_t pService, const uint16_t len, void *pbuf, ISMPport_t portID)
+{
+	int ret = -1;
+  	if ((i2csrvc[portID - PORT_0].pService == NULL) && (pService != NULL)) {
+		i2csrvc[portID - PORT_0].pService = pService;
+		i2csrvc[portID - PORT_0].param = pbuf;
+		i2csrvc[portID - PORT_0].len = len;
+		i2csrvc[portID - PORT_0].rsp = UNKNOWN_RESP;
+		i2csrvc[portID - PORT_0].run = 0;
+		ret = 0;
+	}
+	return ret;
+}
+
+int ICS_delService(ISMPport_t portID)
+{
+	int ret = -1;
+	if ((portID >= PORT_0) && (portID < (PORT_0 + PORT_NUM_MAX))) {
+		i2csrvc[portID - PORT_0].pService = NULL;
+		i2csrvc[portID - PORT_0].param = NULL;
+		i2csrvc[portID - PORT_0].len = 0;
+		i2csrvc[portID - PORT_0].rsp = 0;
+		i2csrvc[portID - PORT_0].run = 0;
+		ret = 0;
+	}
+}
+
+void ICS_run()
+{
+	ISMPport_t portID;
+	for (portID = PORT_0; portID < (PORT_0 + PORT_NUM_MAX); portID++) {
+		if (i2csrvc[portID - PORT_0].pService) {
+			if (i2csrvc[portID - PORT_0].run > 0) {
+				i2csrvc[portID - PORT_0].rsp = i2csrvc[portID - PORT_0].pService(i2csrvc[portID - PORT_0].param);
+				/* Critical Section? */
+				i2csrvc[portID - PORT_0].run--;
+			}
+		}
+	}
 }
